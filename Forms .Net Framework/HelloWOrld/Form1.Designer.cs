bf271b6a1728a76d32e76ab2875437bbb31826e2
Leon Hoffman-Krohn <0511LEHO@gapp.uddevalla.se>
2022-08-25 11:10:39 +0200

    btn

--- Forms .Net Framework/HelloWOrld/Form1.Designer.cs
+++ Forms .Net Framework/HelloWOrld/Form1.Designer.cs
@@ -28,13 +28,33 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnWake = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnWake
+            // 
+            this.btnWake.Location = new System.Drawing.Point(131, 64);
+            this.btnWake.Name = "btnWake";
+            this.btnWake.Size = new System.Drawing.Size(75, 23);
+            this.btnWake.TabIndex = 0;
+            this.btnWake.Text = "Vakna!";
+            this.btnWake.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(884, 861);
+            this.Controls.Add(this.btnWake);
+            this.Name = "Form1";
+            this.Text = "Hello World";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnWake;
     }
 }
 
