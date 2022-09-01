059d0e1d01b85f411ba251d00cd26d1c0bf36719
Leon Hoffman-Krohn <0511LEHO@gapp.uddevalla.se>
2022-09-01 11:04:01 +0200

    3.3

---
+++ Forms .Net Framework/3.3/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _3._3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void btnKör_Click(object sender, EventArgs e)
+        {
+            double a = double.Parse(tbxA.Text), b = double.Parse(tbxB.Text), c = double.Parse(tbxC.Text);
+            lblSumma.Text = "Summan: " + (a + b + c).ToString();
+            lblMedel.Text = "Medelvärdet:" + ((a+b+c)/3).ToString();
+
+        }
+    }
+}
