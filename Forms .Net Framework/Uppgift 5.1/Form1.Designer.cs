4aaa7359ea8930b93d62917bcaf9276affc4cfae
Grappe88 <leon.h-k@hotmail.com>
2022-09-08 08:41:53 +0200

    Uppgift 5.1

---
+++ Forms .Net Framework/Uppgift 5.1/Form1.Designer.cs
@@ -0,0 +1,265 @@
+﻿namespace Uppgift_5._1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnKör = new System.Windows.Forms.Button();
+            this.tbxTal = new System.Windows.Forms.TextBox();
+            this.lblTal = new System.Windows.Forms.Label();
+            this.lblSvar = new System.Windows.Forms.Label();
+            this.lblAdd1 = new System.Windows.Forms.Label();
+            this.lblMult2 = new System.Windows.Forms.Label();
+            this.lblSubTal = new System.Windows.Forms.Label();
+            this.lblAdd3 = new System.Windows.Forms.Label();
+            this.lblDiv2 = new System.Windows.Forms.Label();
+            this.lblSub6 = new System.Windows.Forms.Label();
+            this.lblSteg = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnKör
+            // 
+            this.btnKör.Location = new System.Drawing.Point(192, 447);
+            this.btnKör.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.btnKör.Name = "btnKör";
+            this.btnKör.Size = new System.Drawing.Size(150, 44);
+            this.btnKör.TabIndex = 0;
+            this.btnKör.Text = "KÖR";
+            this.btnKör.UseVisualStyleBackColor = true;
+            this.btnKör.Click += new System.EventHandler(this.btnKör_Click);
+            // 
+            // tbxTal
+            // 
+            this.tbxTal.Location = new System.Drawing.Point(162, 240);
+            this.tbxTal.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.tbxTal.Name = "tbxTal";
+            this.tbxTal.Size = new System.Drawing.Size(196, 31);
+            this.tbxTal.TabIndex = 1;
+            // 
+            // lblTal
+            // 
+            this.lblTal.AutoSize = true;
+            this.lblTal.Location = new System.Drawing.Point(70, 246);
+            this.lblTal.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.lblTal.Name = "lblTal";
+            this.lblTal.Size = new System.Drawing.Size(79, 25);
+            this.lblTal.TabIndex = 2;
+            this.lblTal.Text = "Ditt tal:";
+            // 
+            // lblSvar
+            // 
+            this.lblSvar.AutoSize = true;
+            this.lblSvar.Location = new System.Drawing.Point(78, 359);
+            this.lblSvar.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.lblSvar.Name = "lblSvar";
+            this.lblSvar.Size = new System.Drawing.Size(75, 25);
+            this.lblSvar.TabIndex = 3;
+            this.lblSvar.Text = "SVAR:";
+            // 
+            // lblAdd1
+            // 
+            this.lblAdd1.AutoSize = true;
+            this.lblAdd1.Location = new System.Drawing.Point(936, 151);
+            this.lblAdd1.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.lblAdd1.Name = "lblAdd1";
+            this.lblAdd1.Size = new System.Drawing.Size(0, 25);
+            this.lblAdd1.TabIndex = 4;
+            // 
+            // lblMult2
+            // 
+            this.lblMult2.AutoSize = true;
+            this.lblMult2.Location = new System.Drawing.Point(936, 207);
+            this.lblMult2.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.lblMult2.Name = "lblMult2";
+            this.lblMult2.Size = new System.Drawing.Size(0, 25);
+            this.lblMult2.TabIndex = 5;
+            // 
+            // lblSubTal
+            // 
+            this.lblSubTal.AutoSize = true;
+            this.lblSubTal.Location = new System.Drawing.Point(936, 484);
+            this.lblSubTal.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.lblSubTal.Name = "lblSubTal";
+            this.lblSubTal.Size = new System.Drawing.Size(0, 25);
+            this.lblSubTal.TabIndex = 6;
+            // 
+            // lblAdd3
+            // 
+            this.lblAdd3.AutoSize = true;
+            this.lblAdd3.Location = new System.Drawing.Point(936, 409);
+            this.lblAdd3.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.lblAdd3.Name = "lblAdd3";
+            this.lblAdd3.Size = new System.Drawing.Size(0, 25);
+            this.lblAdd3.TabIndex = 7;
+            // 
+            // lblDiv2
+            // 
+            this.lblDiv2.AutoSize = true;
+            this.lblDiv2.Location = new System.Drawing.Point(936, 347);
+            this.lblDiv2.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.lblDiv2.Name = "lblDiv2";
+            this.lblDiv2.Size = new System.Drawing.Size(0, 25);
+            this.lblDiv2.TabIndex = 8;
+            // 
+            // lblSub6
+            // 
+            this.lblSub6.AutoSize = true;
+            this.lblSub6.Location = new System.Drawing.Point(936, 272);
+            this.lblSub6.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.lblSub6.Name = "lblSub6";
+            this.lblSub6.Size = new System.Drawing.Size(0, 25);
+            this.lblSub6.TabIndex = 9;
+            // 
+            // lblSteg
+            // 
+            this.lblSteg.AutoSize = true;
+            this.lblSteg.Location = new System.Drawing.Point(786, 82);
+            this.lblSteg.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.lblSteg.Name = "lblSteg";
+            this.lblSteg.Size = new System.Drawing.Size(83, 25);
+            this.lblSteg.TabIndex = 10;
+            this.lblSteg.Text = "-STEG-";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(620, 272);
+            this.label1.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(189, 25);
+            this.label1.TabIndex = 16;
+            this.label1.Text = "Subtrahera med 6:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(714, 347);
+            this.label2.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(92, 25);
+            this.label2.TabIndex = 15;
+            this.label2.Text = "Halvera:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(730, 409);
+            this.label3.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(78, 25);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "Plus 3:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(638, 484);
+            this.label4.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(171, 25);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "Subtrahera talet:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(600, 207);
+            this.label5.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(210, 25);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Multiplicera med två:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(730, 151);
+            this.label6.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(78, 25);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Plus 1:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1080, 661);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.lblSteg);
+            this.Controls.Add(this.lblSub6);
+            this.Controls.Add(this.lblDiv2);
+            this.Controls.Add(this.lblAdd3);
+            this.Controls.Add(this.lblSubTal);
+            this.Controls.Add(this.lblMult2);
+            this.Controls.Add(this.lblAdd1);
+            this.Controls.Add(this.lblSvar);
+            this.Controls.Add(this.lblTal);
+            this.Controls.Add(this.tbxTal);
+            this.Controls.Add(this.btnKör);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnKör;
+        private System.Windows.Forms.TextBox tbxTal;
+        private System.Windows.Forms.Label lblTal;
+        private System.Windows.Forms.Label lblSvar;
+        private System.Windows.Forms.Label lblAdd1;
+        private System.Windows.Forms.Label lblMult2;
+        private System.Windows.Forms.Label lblSubTal;
+        private System.Windows.Forms.Label lblAdd3;
+        private System.Windows.Forms.Label lblDiv2;
+        private System.Windows.Forms.Label lblSub6;
+        private System.Windows.Forms.Label lblSteg;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ Forms .Net Framework/Uppgift 5.1/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Uppgift_5._1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnKör_Click(object sender, EventArgs e)
+        {
+            lblSvar.Text = "SVAR: ";
+            double svar = double.Parse(tbxTal.Text);
+            double tal = svar;
+
+            // string svar = ((2*(tal+1)-6)/2+3-tal).ToString();
+
+            lblAdd1.Text = (++svar).ToString();
+            lblMult2.Text = (svar*2).ToString();
+            lblSub6.Text = (svar*2-6).ToString();
+            lblDiv2.Text = ((svar*2-6)/2).ToString();
+            svar = double.Parse(lblDiv2.Text); //Mellanresultat
+            lblAdd3.Text = (svar+3).ToString();
+            lblSubTal.Text = (svar-tal+3).ToString();
+            lblSvar.Text += lblSubTal.Text;
+
+
+        }
+    }
+}
