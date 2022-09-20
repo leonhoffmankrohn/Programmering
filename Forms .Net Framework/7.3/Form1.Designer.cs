f5707c9fcfe389f4e2c1507157ba42b2ed5767ab
Grappe88 <leon.h-k@hotmail.com>
2022-09-20 15:02:11 +0200

    7.3

---
+++ Forms .Net Framework/7.3/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace _7._3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnKast = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblSvar = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnKast
+            // 
+            this.btnKast.Location = new System.Drawing.Point(298, 207);
+            this.btnKast.Name = "btnKast";
+            this.btnKast.Size = new System.Drawing.Size(75, 23);
+            this.btnKast.TabIndex = 0;
+            this.btnKast.Text = "Kasta tärning";
+            this.btnKast.UseVisualStyleBackColor = true;
+            this.btnKast.Click += new System.EventHandler(this.btnKast_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(255, 150);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Antal prickar:";
+            // 
+            // lblSvar
+            // 
+            this.lblSvar.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblSvar.Location = new System.Drawing.Point(330, 149);
+            this.lblSvar.Name = "lblSvar";
+            this.lblSvar.Size = new System.Drawing.Size(66, 24);
+            this.lblSvar.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblSvar);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnKast);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnKast;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblSvar;
+    }
+}
+
---
+++ Forms .Net Framework/7.3/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _7._3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnKast_Click(object sender, EventArgs e)
+        {
+            Random slump = new Random();
+            int resultat = slump.Next(1, 7);
+            lblSvar.Text = resultat.ToString();
+            if (resultat == 6)
+            {
+                MessageBox.Show("Grattis!");
+            }
+        }
+    }
+}
