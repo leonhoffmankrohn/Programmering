f4b1899bf2f12ed114f7dbdc33e821ba026527b0
Leon Hoffman-Krohn <0511LEHO@gapp.uddevalla.se>
2022-09-20 14:18:02 +0200

    Senaste

--- Forms .Net Framework/Uppgift 5.1/Form1.cs
+++ Forms .Net Framework/Uppgift 5.1/Form1.cs
@@ -24,7 +24,14 @@ namespace Uppgift_5._1
             double tal = svar;
 
             // string svar = ((2*(tal+1)-6)/2+3-tal).ToString();
-
+            svar++;
+            svar *= 2;
+            svar -= 6;
+            svar /= 2;
+            svar += 3;
+            svar -= tal;
+            lblSvar.Text += svar.ToString();
+            /*
             lblAdd1.Text = (++svar).ToString();
             lblMult2.Text = (svar*2).ToString();
             lblSub6.Text = (svar*2-6).ToString();
@@ -33,7 +40,7 @@ namespace Uppgift_5._1
             lblAdd3.Text = (svar+3).ToString();
             lblSubTal.Text = (svar-tal+3).ToString();
             lblSvar.Text += lblSubTal.Text;
-
+            */
 
         }
     }
