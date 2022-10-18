816316aff7d86c2c47c282f503853abf0194f657
Leon Hoffman-Krohn <0511LEHO@gapp.uddevalla.se>
2022-10-18 15:34:57 +0200

    9.4

---
+++ Forms .Net Framework/9.4/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace _9._4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbxUtdata = new System.Windows.Forms.TextBox();
+            this.btnKör = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // tbxUtdata
+            // 
+            this.tbxUtdata.Location = new System.Drawing.Point(234, 63);
+            this.tbxUtdata.Multiline = true;
+            this.tbxUtdata.Name = "tbxUtdata";
+            this.tbxUtdata.Size = new System.Drawing.Size(295, 150);
+            this.tbxUtdata.TabIndex = 0;
+            // 
+            // btnKör
+            // 
+            this.btnKör.Location = new System.Drawing.Point(377, 257);
+            this.btnKör.Name = "btnKör";
+            this.btnKör.Size = new System.Drawing.Size(75, 23);
+            this.btnKör.TabIndex = 1;
+            this.btnKör.Text = "KÖR";
+            this.btnKör.UseVisualStyleBackColor = true;
+            this.btnKör.Click += new System.EventHandler(this.btnKör_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnKör);
+            this.Controls.Add(this.tbxUtdata);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbxUtdata;
+        private System.Windows.Forms.Button btnKör;
+    }
+}
+
