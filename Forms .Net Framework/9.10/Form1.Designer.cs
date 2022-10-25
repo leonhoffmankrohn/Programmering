c7fb3fc86dab0e94a1e344e6e9262bf00ac0c755
Leon Hoffman-Krohn <0511LEHO@gapp.uddevalla.se>
2022-10-25 15:40:40 +0200

    9.10

---
+++ Forms .Net Framework/9.10/Form1.Designer.cs
@@ -0,0 +1,260 @@
+﻿namespace _9._10
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.tbxRad1 = new System.Windows.Forms.TextBox();
+            this.tbxRad2 = new System.Windows.Forms.TextBox();
+            this.tbxVal = new System.Windows.Forms.TextBox();
+            this.btnGenerera = new System.Windows.Forms.Button();
+            this.btnVälj = new System.Windows.Forms.Button();
+            this.lbxMeny = new System.Windows.Forms.ListBox();
+            this.tbxRad3 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(4, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Styrka";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(4, 41);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Smidighet";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(4, 54);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(34, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Hälsa";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(4, 67);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(54, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Intelligens";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(4, 80);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(54, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Viljestyrka";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(4, 93);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(44, 13);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Karisma";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(60, 6);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(36, 13);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Rad 1";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(101, 6);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(36, 13);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "Rad 2";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(137, 6);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(36, 13);
+            this.label9.TabIndex = 8;
+            this.label9.Text = "Rad 3";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(194, 66);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(33, 13);
+            this.label10.TabIndex = 9;
+            this.label10.Text = "Meny";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(274, 6);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(22, 13);
+            this.label11.TabIndex = 10;
+            this.label11.Text = "Val";
+            // 
+            // tbxRad1
+            // 
+            this.tbxRad1.Location = new System.Drawing.Point(62, 22);
+            this.tbxRad1.Multiline = true;
+            this.tbxRad1.Name = "tbxRad1";
+            this.tbxRad1.Size = new System.Drawing.Size(33, 86);
+            this.tbxRad1.TabIndex = 11;
+            // 
+            // tbxRad2
+            // 
+            this.tbxRad2.Location = new System.Drawing.Point(101, 22);
+            this.tbxRad2.Multiline = true;
+            this.tbxRad2.Name = "tbxRad2";
+            this.tbxRad2.Size = new System.Drawing.Size(33, 86);
+            this.tbxRad2.TabIndex = 12;
+            // 
+            // tbxVal
+            // 
+            this.tbxVal.Location = new System.Drawing.Point(277, 22);
+            this.tbxVal.Multiline = true;
+            this.tbxVal.Name = "tbxVal";
+            this.tbxVal.Size = new System.Drawing.Size(38, 129);
+            this.tbxVal.TabIndex = 13;
+            // 
+            // btnGenerera
+            // 
+            this.btnGenerera.Location = new System.Drawing.Point(7, 131);
+            this.btnGenerera.Name = "btnGenerera";
+            this.btnGenerera.Size = new System.Drawing.Size(75, 23);
+            this.btnGenerera.TabIndex = 14;
+            this.btnGenerera.Text = "Generera";
+            this.btnGenerera.UseVisualStyleBackColor = true;
+            // 
+            // btnVälj
+            // 
+            this.btnVälj.Location = new System.Drawing.Point(197, 131);
+            this.btnVälj.Name = "btnVälj";
+            this.btnVälj.Size = new System.Drawing.Size(57, 23);
+            this.btnVälj.TabIndex = 15;
+            this.btnVälj.Text = "Välj";
+            this.btnVälj.UseVisualStyleBackColor = true;
+            // 
+            // lbxMeny
+            // 
+            this.lbxMeny.FormattingEnabled = true;
+            this.lbxMeny.Location = new System.Drawing.Point(197, 82);
+            this.lbxMeny.Name = "lbxMeny";
+            this.lbxMeny.Size = new System.Drawing.Size(57, 43);
+            this.lbxMeny.TabIndex = 16;
+            // 
+            // tbxRad3
+            // 
+            this.tbxRad3.Location = new System.Drawing.Point(139, 22);
+            this.tbxRad3.Multiline = true;
+            this.tbxRad3.Name = "tbxRad3";
+            this.tbxRad3.Size = new System.Drawing.Size(33, 86);
+            this.tbxRad3.TabIndex = 17;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(335, 173);
+            this.Controls.Add(this.tbxRad3);
+            this.Controls.Add(this.lbxMeny);
+            this.Controls.Add(this.btnVälj);
+            this.Controls.Add(this.btnGenerera);
+            this.Controls.Add(this.tbxVal);
+            this.Controls.Add(this.tbxRad2);
+            this.Controls.Add(this.tbxRad1);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.TextBox tbxRad1;
+        private System.Windows.Forms.TextBox tbxRad2;
+        private System.Windows.Forms.TextBox tbxVal;
+        private System.Windows.Forms.Button btnGenerera;
+        private System.Windows.Forms.Button btnVälj;
+        private System.Windows.Forms.ListBox lbxMeny;
+        private System.Windows.Forms.TextBox tbxRad3;
+    }
+}
+
