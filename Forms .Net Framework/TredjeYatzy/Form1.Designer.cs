65949cf40d0866761ddf79ea6f73d172428864f6
Leon Hoffman-Krohn <0511LEHO@gapp.uddevalla.se>
2022-12-08 11:40:19 +0100

    Forts

---
+++ Forms .Net Framework/TredjeYatzy/Form1.Designer.cs
@@ -0,0 +1,388 @@
+﻿namespace TredjeYatzy
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pc1 = new System.Windows.Forms.PictureBox();
+            this.pc5 = new System.Windows.Forms.PictureBox();
+            this.pc4 = new System.Windows.Forms.PictureBox();
+            this.pc3 = new System.Windows.Forms.PictureBox();
+            this.pc2 = new System.Windows.Forms.PictureBox();
+            this.btnSlå = new System.Windows.Forms.Button();
+            this.lbxVälj = new System.Windows.Forms.ListBox();
+            this.lbxMöjligRes = new System.Windows.Forms.ListBox();
+            this.lbxValdRes = new System.Windows.Forms.ListBox();
+            this.gbxResultat = new System.Windows.Forms.GroupBox();
+            this.lblOmgångar = new System.Windows.Forms.Label();
+            this.btnVälj = new System.Windows.Forms.Button();
+            this.prbarKastkvar = new System.Windows.Forms.ProgressBar();
+            this.lblTotala = new System.Windows.Forms.Label();
+            this.btnStäng = new System.Windows.Forms.Button();
+            this.btnRestart = new System.Windows.Forms.Button();
+            this.lbxAndraFörsök = new System.Windows.Forms.ListBox();
+            this.tabAllt = new System.Windows.Forms.TabControl();
+            this.tabPYatzy = new System.Windows.Forms.TabPage();
+            this.tabPResultat = new System.Windows.Forms.TabPage();
+            ((System.ComponentModel.ISupportInitialize)(this.pc1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pc5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pc4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pc3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pc2)).BeginInit();
+            this.gbxResultat.SuspendLayout();
+            this.tabAllt.SuspendLayout();
+            this.tabPYatzy.SuspendLayout();
+            this.tabPResultat.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pc1
+            // 
+            this.pc1.Image = global::TredjeYatzy.Tärningsgrafik.d0;
+            this.pc1.Location = new System.Drawing.Point(33, 243);
+            this.pc1.Name = "pc1";
+            this.pc1.Size = new System.Drawing.Size(50, 50);
+            this.pc1.TabIndex = 0;
+            this.pc1.TabStop = false;
+            this.pc1.Click += new System.EventHandler(this.pc1_Click);
+            // 
+            // pc5
+            // 
+            this.pc5.Image = global::TredjeYatzy.Tärningsgrafik.d0;
+            this.pc5.Location = new System.Drawing.Point(33, 19);
+            this.pc5.Name = "pc5";
+            this.pc5.Size = new System.Drawing.Size(50, 50);
+            this.pc5.TabIndex = 1;
+            this.pc5.TabStop = false;
+            this.pc5.Click += new System.EventHandler(this.pc5_Click);
+            // 
+            // pc4
+            // 
+            this.pc4.Image = global::TredjeYatzy.Tärningsgrafik.d0;
+            this.pc4.Location = new System.Drawing.Point(33, 75);
+            this.pc4.Name = "pc4";
+            this.pc4.Size = new System.Drawing.Size(50, 50);
+            this.pc4.TabIndex = 2;
+            this.pc4.TabStop = false;
+            this.pc4.Click += new System.EventHandler(this.pc4_Click);
+            // 
+            // pc3
+            // 
+            this.pc3.Image = global::TredjeYatzy.Tärningsgrafik.d0;
+            this.pc3.Location = new System.Drawing.Point(33, 131);
+            this.pc3.Name = "pc3";
+            this.pc3.Size = new System.Drawing.Size(50, 50);
+            this.pc3.TabIndex = 3;
+            this.pc3.TabStop = false;
+            this.pc3.Click += new System.EventHandler(this.pc3_Click);
+            // 
+            // pc2
+            // 
+            this.pc2.Image = global::TredjeYatzy.Tärningsgrafik.d0;
+            this.pc2.Location = new System.Drawing.Point(33, 187);
+            this.pc2.Name = "pc2";
+            this.pc2.Size = new System.Drawing.Size(50, 50);
+            this.pc2.TabIndex = 4;
+            this.pc2.TabStop = false;
+            this.pc2.Click += new System.EventHandler(this.pc2_Click);
+            // 
+            // btnSlå
+            // 
+            this.btnSlå.Location = new System.Drawing.Point(8, 313);
+            this.btnSlå.Name = "btnSlå";
+            this.btnSlå.Size = new System.Drawing.Size(75, 44);
+            this.btnSlå.TabIndex = 5;
+            this.btnSlå.Text = "SLÅ";
+            this.btnSlå.UseVisualStyleBackColor = true;
+            this.btnSlå.Click += new System.EventHandler(this.btnSlå_Click);
+            // 
+            // lbxVälj
+            // 
+            this.lbxVälj.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lbxVälj.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbxVälj.FormattingEnabled = true;
+            this.lbxVälj.ItemHeight = 25;
+            this.lbxVälj.Items.AddRange(new object[] {
+            "Ettor:",
+            "Tvåor:",
+            "Treor:",
+            "Fyror:",
+            "Femor:",
+            "Sexor:",
+            "Par:",
+            "Två par:",
+            "Triss:",
+            "Kåk:",
+            "Liten stege:",
+            "Stor stege:",
+            "Fyrtal:",
+            "Yatzy:"});
+            this.lbxVälj.Location = new System.Drawing.Point(3, 16);
+            this.lbxVälj.Name = "lbxVälj";
+            this.lbxVälj.Size = new System.Drawing.Size(220, 372);
+            this.lbxVälj.TabIndex = 6;
+            // 
+            // lbxMöjligRes
+            // 
+            this.lbxMöjligRes.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbxMöjligRes.FormattingEnabled = true;
+            this.lbxMöjligRes.ItemHeight = 25;
+            this.lbxMöjligRes.Items.AddRange(new object[] {
+            "0",
+            "0",
+            "0",
+            "0",
+            "0",
+            "0",
+            "0",
+            "0",
+            "0",
+            "0",
+            "0",
+            "0",
+            "0",
+            "0"});
+            this.lbxMöjligRes.Location = new System.Drawing.Point(123, 16);
+            this.lbxMöjligRes.Name = "lbxMöjligRes";
+            this.lbxMöjligRes.SelectionMode = System.Windows.Forms.SelectionMode.None;
+            this.lbxMöjligRes.Size = new System.Drawing.Size(41, 354);
+            this.lbxMöjligRes.TabIndex = 7;
+            // 
+            // lbxValdRes
+            // 
+            this.lbxValdRes.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbxValdRes.FormattingEnabled = true;
+            this.lbxValdRes.ItemHeight = 25;
+            this.lbxValdRes.Items.AddRange(new object[] {
+            "0",
+            "0",
+            "0",
+            "0",
+            "0",
+            "0",
+            "0",
+            "0",
+            "0",
+            "0",
+            "0",
+            "0",
+            "0",
+            "0"});
+            this.lbxValdRes.Location = new System.Drawing.Point(170, 16);
+            this.lbxValdRes.Name = "lbxValdRes";
+            this.lbxValdRes.SelectionMode = System.Windows.Forms.SelectionMode.None;
+            this.lbxValdRes.Size = new System.Drawing.Size(41, 354);
+            this.lbxValdRes.TabIndex = 8;
+            // 
+            // gbxResultat
+            // 
+            this.gbxResultat.Controls.Add(this.lblOmgångar);
+            this.gbxResultat.Controls.Add(this.lbxValdRes);
+            this.gbxResultat.Controls.Add(this.lbxMöjligRes);
+            this.gbxResultat.Controls.Add(this.lbxVälj);
+            this.gbxResultat.Enabled = false;
+            this.gbxResultat.Location = new System.Drawing.Point(89, 19);
+            this.gbxResultat.Name = "gbxResultat";
+            this.gbxResultat.Size = new System.Drawing.Size(226, 391);
+            this.gbxResultat.TabIndex = 9;
+            this.gbxResultat.TabStop = false;
+            this.gbxResultat.Text = "Resultaten";
+            // 
+            // lblOmgångar
+            // 
+            this.lblOmgångar.Location = new System.Drawing.Point(37, 373);
+            this.lblOmgångar.Name = "lblOmgångar";
+            this.lblOmgångar.Size = new System.Drawing.Size(103, 15);
+            this.lblOmgångar.TabIndex = 16;
+            this.lblOmgångar.Text = "Omgångar kvar: 14";
+            // 
+            // btnVälj
+            // 
+            this.btnVälj.Enabled = false;
+            this.btnVälj.Location = new System.Drawing.Point(8, 363);
+            this.btnVälj.Name = "btnVälj";
+            this.btnVälj.Size = new System.Drawing.Size(75, 44);
+            this.btnVälj.TabIndex = 10;
+            this.btnVälj.Text = "Välj/Kasta igen";
+            this.btnVälj.UseVisualStyleBackColor = true;
+            this.btnVälj.Click += new System.EventHandler(this.btnVälj_Click);
+            // 
+            // prbarKastkvar
+            // 
+            this.prbarKastkvar.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
+            this.prbarKastkvar.ForeColor = System.Drawing.Color.Lime;
+            this.prbarKastkvar.Location = new System.Drawing.Point(8, 347);
+            this.prbarKastkvar.Maximum = 3;
+            this.prbarKastkvar.Name = "prbarKastkvar";
+            this.prbarKastkvar.Size = new System.Drawing.Size(75, 10);
+            this.prbarKastkvar.Step = -1;
+            this.prbarKastkvar.Style = System.Windows.Forms.ProgressBarStyle.Continuous;
+            this.prbarKastkvar.TabIndex = 11;
+            this.prbarKastkvar.Value = 3;
+            // 
+            // lblTotala
+            // 
+            this.lblTotala.BackColor = System.Drawing.Color.MediumSeaGreen;
+            this.lblTotala.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lblTotala.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTotala.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.lblTotala.Location = new System.Drawing.Point(3, 27);
+            this.lblTotala.Name = "lblTotala";
+            this.lblTotala.Size = new System.Drawing.Size(247, 354);
+            this.lblTotala.TabIndex = 9;
+            this.lblTotala.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // btnStäng
+            // 
+            this.btnStäng.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.btnStäng.Location = new System.Drawing.Point(3, 381);
+            this.btnStäng.Name = "btnStäng";
+            this.btnStäng.Size = new System.Drawing.Size(320, 30);
+            this.btnStäng.TabIndex = 13;
+            this.btnStäng.Text = "Lämna spelet";
+            this.btnStäng.UseVisualStyleBackColor = true;
+            this.btnStäng.Click += new System.EventHandler(this.btnStäng_Click);
+            // 
+            // btnRestart
+            // 
+            this.btnRestart.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnRestart.Location = new System.Drawing.Point(3, 3);
+            this.btnRestart.Name = "btnRestart";
+            this.btnRestart.Size = new System.Drawing.Size(320, 24);
+            this.btnRestart.TabIndex = 14;
+            this.btnRestart.Text = "Starta om spel!";
+            this.btnRestart.UseVisualStyleBackColor = true;
+            this.btnRestart.Click += new System.EventHandler(this.btnRestart_Click);
+            // 
+            // lbxAndraFörsök
+            // 
+            this.lbxAndraFörsök.Dock = System.Windows.Forms.DockStyle.Right;
+            this.lbxAndraFörsök.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbxAndraFörsök.FormattingEnabled = true;
+            this.lbxAndraFörsök.ItemHeight = 20;
+            this.lbxAndraFörsök.Location = new System.Drawing.Point(250, 27);
+            this.lbxAndraFörsök.Name = "lbxAndraFörsök";
+            this.lbxAndraFörsök.SelectionMode = System.Windows.Forms.SelectionMode.None;
+            this.lbxAndraFörsök.Size = new System.Drawing.Size(73, 354);
+            this.lbxAndraFörsök.TabIndex = 15;
+            // 
+            // tabAllt
+            // 
+            this.tabAllt.Appearance = System.Windows.Forms.TabAppearance.FlatButtons;
+            this.tabAllt.Controls.Add(this.tabPYatzy);
+            this.tabAllt.Controls.Add(this.tabPResultat);
+            this.tabAllt.Cursor = System.Windows.Forms.Cursors.Default;
+            this.tabAllt.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabAllt.Location = new System.Drawing.Point(0, 0);
+            this.tabAllt.Multiline = true;
+            this.tabAllt.Name = "tabAllt";
+            this.tabAllt.SelectedIndex = 0;
+            this.tabAllt.Size = new System.Drawing.Size(334, 443);
+            this.tabAllt.TabIndex = 16;
+            this.tabAllt.TabStop = false;
+            this.tabAllt.Selecting += new System.Windows.Forms.TabControlCancelEventHandler(this.tabAllt_Selecting);
+            // 
+            // tabPYatzy
+            // 
+            this.tabPYatzy.BackColor = System.Drawing.SystemColors.Control;
+            this.tabPYatzy.Controls.Add(this.gbxResultat);
+            this.tabPYatzy.Controls.Add(this.btnVälj);
+            this.tabPYatzy.Controls.Add(this.prbarKastkvar);
+            this.tabPYatzy.Controls.Add(this.pc4);
+            this.tabPYatzy.Controls.Add(this.pc2);
+            this.tabPYatzy.Controls.Add(this.pc5);
+            this.tabPYatzy.Controls.Add(this.pc1);
+            this.tabPYatzy.Controls.Add(this.pc3);
+            this.tabPYatzy.Controls.Add(this.btnSlå);
+            this.tabPYatzy.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.tabPYatzy.Location = new System.Drawing.Point(4, 25);
+            this.tabPYatzy.Name = "tabPYatzy";
+            this.tabPYatzy.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPYatzy.Size = new System.Drawing.Size(326, 414);
+            this.tabPYatzy.TabIndex = 0;
+            this.tabPYatzy.Text = "YATZY";
+            // 
+            // tabPResultat
+            // 
+            this.tabPResultat.BackColor = System.Drawing.SystemColors.Control;
+            this.tabPResultat.Controls.Add(this.lblTotala);
+            this.tabPResultat.Controls.Add(this.lbxAndraFörsök);
+            this.tabPResultat.Controls.Add(this.btnRestart);
+            this.tabPResultat.Controls.Add(this.btnStäng);
+            this.tabPResultat.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.tabPResultat.Location = new System.Drawing.Point(4, 25);
+            this.tabPResultat.Name = "tabPResultat";
+            this.tabPResultat.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPResultat.Size = new System.Drawing.Size(326, 414);
+            this.tabPResultat.TabIndex = 1;
+            this.tabPResultat.Text = "RESULTAT";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(334, 443);
+            this.Controls.Add(this.tabAllt);
+            this.Name = "Form1";
+            this.Text = "visual";
+            ((System.ComponentModel.ISupportInitialize)(this.pc1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pc5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pc4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pc3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pc2)).EndInit();
+            this.gbxResultat.ResumeLayout(false);
+            this.tabAllt.ResumeLayout(false);
+            this.tabPYatzy.ResumeLayout(false);
+            this.tabPResultat.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pc1;
+        private System.Windows.Forms.PictureBox pc5;
+        private System.Windows.Forms.PictureBox pc4;
+        private System.Windows.Forms.PictureBox pc3;
+        private System.Windows.Forms.PictureBox pc2;
+        private System.Windows.Forms.Button btnSlå;
+        private System.Windows.Forms.ListBox lbxVälj;
+        private System.Windows.Forms.ListBox lbxMöjligRes;
+        private System.Windows.Forms.ListBox lbxValdRes;
+        private System.Windows.Forms.GroupBox gbxResultat;
+        private System.Windows.Forms.Button btnVälj;
+        private System.Windows.Forms.ProgressBar prbarKastkvar;
+        private System.Windows.Forms.Label lblTotala;
+        private System.Windows.Forms.Button btnStäng;
+        private System.Windows.Forms.Button btnRestart;
+        private System.Windows.Forms.ListBox lbxAndraFörsök;
+        private System.Windows.Forms.Label lblOmgångar;
+        private System.Windows.Forms.TabControl tabAllt;
+        private System.Windows.Forms.TabPage tabPYatzy;
+        private System.Windows.Forms.TabPage tabPResultat;
+    }
+}
+
---
+++ Forms .Net Framework/TredjeYatzy/Form1.cs
@@ -0,0 +1,236 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TredjeYatzy
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            tabAllt.SelectedTab = tabPYatzy;
+            PictureBox[] ramar = {pc1, pc2, pc3, pc4, pc5};
+            for (int i = 0; i < ramar.Length; i++)
+            {
+                ramar[i].Click += allPc_Click;
+            }
+        }
+
+        // Varsitt index är en tärning
+        bool[] behåll = new bool[5];
+        Image[] grafik = { Tärningsgrafik.d0, Tärningsgrafik.d1, Tärningsgrafik.d2, Tärningsgrafik.d3, Tärningsgrafik.d4, Tärningsgrafik.d5, Tärningsgrafik.d6 };
+        int[] slag = new int[5];
+        Random generator = new Random();
+        int kastKvar = 3;
+        int speladeSpel = 0;
+        int[] resultatVärden = new int[14]; // ettor, tvåor, treor, fyror, femor, sexor, par, två-par, triss, kåk, liten-stege, stor-stege, yatzy
+        int[] tagnaSvar = new int[14]; // ""--""
+        bool dator = false;
+
+
+
+        void ändraBehåll(int i) //Denna metod ändrar utseende och ändrar om behåll[i] ska vara true elr false när vi trycker på knapparna.
+        {
+            PictureBox[] ramar = { pc1, pc2, pc3, pc4, pc5 };
+            
+            if (ramar[i].BorderStyle == BorderStyle.None && kastKvar < 3)
+            { behåll[i] = true; ramar[i].BorderStyle = BorderStyle.FixedSingle; }
+            else
+            { behåll[i] = false; ramar[i].BorderStyle = BorderStyle.None; }
+            
+        }
+
+        /*ändra behåll bool för indexet 1-5 
+         * 
+         */
+
+        void kastaTärningar() 
+        {
+            PictureBox[] ramar = { pc1, pc2, pc3, pc4, pc5 };
+            
+            if (kastKvar > 0) // Kollar först om vi har något kast kvar för att kasta
+            {
+                // Slår tärningarna som inte vi har valt att behålla
+                for (int i = 0; i < 5; i++)
+                {
+                    if (!behåll[i]) slag[i] = generator.Next(1, 7);
+                    ramar[i].Image = grafik[slag[i]];
+                }
+                kastKvar--;
+                prbarKastkvar.PerformStep();
+            }
+        }
+
+        void beräknaResultat()
+        {
+            int antalPar = 0;
+            // Du kan inte slå mer men du får välja och använda resultatrutan
+            btnSlå.Enabled = false;
+            gbxResultat.Enabled = true;
+            btnVälj.Enabled = true;
+
+            // Loopar för varje möjlig sida
+            for (int i = 1; i <= 6; i++)
+            {
+                int lika = 0;
+
+                // loopar de tärningar vi har för att se om de matchar
+                for (int j = 0; j < slag.Length; j++)
+                {// Kollar om det slagits ett speciellt nummer
+                    if (i == slag[j]) lika++;
+                }
+
+                // Sparar antal för (1 -> 6)
+                resultatVärden[i - 1] = lika * i;
+
+                // Detta segment kollar bara antalet lika och säger vad det innebär
+                if (lika >= 2) // Här kollar den om det finns ett par på nummer i
+                {
+                    antalPar++;
+                    if (antalPar == 2) { resultatVärden[7] = resultatVärden[6] + i * 2; };// Två par 
+                    resultatVärden[6] = i * 2; // Största par
+                }
+                if (lika >= 3) { resultatVärden[8] = i * 3; } //Triss
+                if (lika >= 4) { resultatVärden[12] = i * 4; } // Fyrtal
+                if (lika == 5) { resultatVärden[13] = 50; } // Yatzy
+
+            }
+
+            // Utanför loopen eftersom de inte beror direkt på antalet lika eller nuvarande sida på tärningen.
+            // Detta till hänsyn till prestanda för att skapa vana till större projekt
+            if (!slag.Contains(1) && antalPar == 0) resultatVärden[11] = 20; // Stor stege
+            else if (!slag.Contains(6) && antalPar == 0) resultatVärden[10] = 15; // Liten stege
+            if (antalPar == 2 && resultatVärden[8] != 0) resultatVärden[9] = resultatVärden[7] - 2 * resultatVärden[8] / 3 + resultatVärden[8]; // Kåk.
+        }
+
+        private void btnSlå_Click(object sender, EventArgs e)
+        {
+            // Kastar tärningarna
+            kastaTärningar();
+
+            // Sätter igång resultatberäkning
+            bool visaResultat = (kastKvar == 0) ? true : false;
+
+            // Nollställer alla möjliga resultatvärden
+            for (int i = 0; i < resultatVärden.Length; i++) resultatVärden[i] = 0;
+
+
+            // Visar bara resultat om personen har slagit 3 ggr
+            if (visaResultat)
+            {
+                beräknaResultat();
+
+                // Uppdatera grafiken för de möjliga resultaten
+                for (int i = 0; i < resultatVärden.Length; i++)
+                {
+                    lbxMöjligRes.Items[i] = resultatVärden[i];
+                }
+            }
+        }
+        private void btnVälj_Click(object sender, EventArgs e)
+        {
+            if (lbxVälj.SelectedIndex != -1 && int.Parse(lbxMöjligRes.Items[lbxVälj.SelectedIndex].ToString()) != 0)
+            {
+                // Spara valt resultat 
+                int index = lbxVälj.SelectedIndex;
+                tagnaSvar[index] = resultatVärden[index];
+
+                // Uppdatera grafik för lbxValdRes
+                for (int i = 0; i < tagnaSvar.Length; i++) lbxValdRes.Items[i] = tagnaSvar[i];
+
+                // Nollställ värden för möjliga värden
+                for (int i = 0; i < resultatVärden.Length; i++)
+                {
+                    resultatVärden[i] = 0;
+                    lbxMöjligRes.Items[i] = 0;
+                }
+                lbxVälj.ClearSelected();
+
+                kastKvar = 3;
+                prbarKastkvar.Value = 3;
+                btnSlå.Enabled = true;
+                btnVälj.Enabled = false;
+                gbxResultat.Enabled = false;
+
+                // Nollställer värdena och grafiken för tärningarna
+                PictureBox[] ramar = { pc1, pc2, pc3, pc4, pc5 };
+                for (int i = 0; i < slag.Length; i++)
+                {
+                    slag[i] = 0;
+                    ramar[i].Image = grafik[0];
+                    ramar[i].BorderStyle = BorderStyle.None;
+                    behåll[i] = false;
+
+                }
+
+                speladeSpel++;
+                if (speladeSpel == 14)
+                {
+                    dator = true;
+                    tabAllt.SelectTab(1);
+
+                    // Visa grön skärm och resultat
+                    int resultat = tagnaSvar.Sum();
+                    lblTotala.Text = "Grattis!\r\n" + "Du samlade totalt ihop " + resultat + " poäng!";
+                    lbxAndraFörsök.Items.Add(resultat);
+
+                    // Rensa poäng
+                    for (int i = 0; i < tagnaSvar.Length; i++)
+                    {
+                        tagnaSvar[i] = 0;
+                        lbxValdRes.Items[i] = 0;
+                    }
+                }
+
+                lblOmgångar.Text = "Omgångar kvar: " + (14 - speladeSpel);
+            }
+            else // Varnar om man inte valt ett resultat
+            {
+                MessageBox.Show("Sorry, men du måste välja ett resultat att spara...");
+
+            }
+        }
+
+        private void tabAllt_Selecting(object sender, TabControlCancelEventArgs e)
+        {
+            //Tillåter inte att spelaren byter tabb
+            e.Cancel = (speladeSpel == 14 && dator) ? false : true;
+            dator = false;
+        }
+
+        private void btnRestart_Click(object sender, EventArgs e)
+        {
+            //      Ändra visible på allt till true
+            dator = true;
+            tabAllt.SelectTab(0);
+            speladeSpel = 0;
+
+            lblOmgångar.Text = "Omgångar kvar: 14";
+        }
+
+        private void btnStäng_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        // Här kommer alla olika tärningar och hur vi sparar dem till nästa slag 
+        private void pc1_Click(object sender, EventArgs e) { ändraBehåll(0); }
+        private void pc2_Click(object sender, EventArgs e) { ändraBehåll(1); }
+        private void pc3_Click(object sender, EventArgs e) { ändraBehåll(2); }
+        private void pc4_Click(object sender, EventArgs e) { ändraBehåll(3); }
+        private void pc5_Click(object sender, EventArgs e) { ändraBehåll(4); }
+
+        private void allPc_Click(object sender, EventArgs e)
+        {
+            //Fortsättning följer...
+        }
+    }
+}
