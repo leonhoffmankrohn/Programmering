2777e223f604af32e9d7b246aeabec9d26f8fb9d
Leon Hoffman-Krohn <0511LEHO@gapp.uddevalla.se>
2023-02-17 11:48:12 +0100

    Inte klar

---
+++ Forms .Net Framework/Filmregister/Film.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Filmregister
+{
+    internal class Film
+    {
+        /*
+            Filmregistret ska ta in film och dens namn i konstruktör.
+            Man ska kunna visa årgång, skådespelare, genre, regissör och revenue.
+        */
+        List<string> skådespelare = new List<string>();
+        string genre;
+        string regissör;
+        int profit;
+
+        public int Årgång { get; }
+
+        public string Genre 
+        {
+            get 
+            {
+                return genre;
+            }
+            set 
+            {
+                genre = value;
+            }
+        }
+        public string Regissör { }
+    }
+}
