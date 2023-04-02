636fa8b46c7ddbe224fb6522c8e68280387dae3f
Grappe88 <leon.h-k@hotmail.com>
2023-04-02 21:50:44 +0200

    Den skriver ut två textbox efter varandra och med gap till nästa, prompt och definition forts sen

--- Forms .Net Framework/HoffQuiz/MainWindow.xaml.cs
+++ Forms .Net Framework/HoffQuiz/MainWindow.xaml.cs
@@ -24,50 +24,23 @@ namespace HoffQuiz
         {
             InitializeComponent();
         }
+        List<User> users = new List<User>();
+        List<Quiz> quizzes = new List<Quiz>();
 
-        List<TextBox> textBoxes = new();
-
-        private void Reset()
+        private void UpdateCreationInterface()
         {
-            for (int i = 0; i < textBoxes.Count; i++)
-            {
-                stackCreate.Children.Remove(textBoxes[i]);
-            }
-        }
-
-        private void Update()
-        {
-            stackCreate.Children.Clear();
-            for (int i = 0; i < textBoxes.Count; i++)
-            {
-                stackCreate.Children.Add(textBoxes[i]);
-                if (i == 0) textBoxes[i].VerticalAlignment = VerticalAlignment.Center;
-
-            }
-        }
-
-        private void CreateVisual()
-        {
-            TextBox textBox = new();
-            textBox.Padding = new Thickness(5);
-            textBox.Margin = new Thickness(0, 20, 0, 20);
-            textBox.Width = 500;
-            textBox.Text = "Här kan du skriva lite dittan och dattan, test";
-            textBox.HorizontalAlignment = HorizontalAlignment.Center;
-
-            textBoxes.Add(textBox);
-        }
-
-
-        private void Create()
-        {
-            CreateVisual();
-            Update();
+            
         }
 
         private void btnNew_Click(object sender, RoutedEventArgs e)
         {
-            Create();
+            quizzes.Add(new Quiz("Hoff Test 1"));
+            quizzes[0].NewSimQ(stackCreate);
+        }
+
+        private void tabQuizCreate_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            Console.WriteLine("Hej");
         }
     }
 }
---
+++ Forms .Net Framework/HoffQuiz/Quiz.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace HoffQuiz
+{
+    class Quiz
+    {
+        public List<Question> Questions = new();
+
+        public string Name { get; set; }
+
+        public Quiz(string _name)
+        {
+            Name = _name;
+        }
+        public void NewSimQ(StackPanel panel)
+        {
+            Questions.Add(new Simple(panel));
+        }
+        public void NewMultQ()
+        {
+            Questions.Add(new MultipleChoice());
+        }
+        public void NewPicQ()
+        {
+            Questions.Add(new PictureQ());
+        }
+        public void NewMathQ()
+        {
+            Questions.Add(new MathQ());
+        }
+
+    }
+
+    class Question
+    {
+        //Prompt
+        //Definition
+
+        public virtual void Initialize(StackPanel panel) { }
+        public virtual void Save() { }
+    }
+
+    class Simple : Question
+    {
+        string prompt;
+        string definition;
+
+        public Simple(StackPanel _panel)
+        {
+            Initialize(_panel);
+        }
+        public override void Initialize(StackPanel panel)
+        {
+            TextBox tbxPrompt = new TextBox
+            {
+                Padding = new Thickness(5),
+                Margin = new Thickness(0, 20, 0, 5),
+                Width = 500,
+                Text = "Här kan du skriva lite dittan och dattan, test",
+                HorizontalAlignment = HorizontalAlignment.Center
+            };
+            TextBox tbxDefinition = new TextBox
+            {
+                Padding = new Thickness(5),
+                Margin = new Thickness(0, 5, 0, 20),
+                Width = 500,
+                Text = "Här kan du skriva lite dittan och dattan, test",
+                HorizontalAlignment = HorizontalAlignment.Center
+            };
+            panel.Children.Add(tbxPrompt);
+            panel.Children.Add(tbxDefinition);
+        }
+
+    }
+    class MultipleChoice : Question
+    {
+
+    }
+    class PictureQ : Question
+    {
+
+    }
+    class MathQ : Question
+    {
+
+    }
+}
