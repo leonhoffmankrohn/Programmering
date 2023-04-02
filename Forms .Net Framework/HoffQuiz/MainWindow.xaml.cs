28b250c49020ddce182a2c3d781b3e3948a1d6ec
Grappe88 <leon.h-k@hotmail.com>
2023-04-02 19:45:41 +0200

    Lite mer på designen

--- Forms .Net Framework/HoffQuiz/MainWindow.xaml.cs
+++ Forms .Net Framework/HoffQuiz/MainWindow.xaml.cs
@@ -24,5 +24,50 @@ namespace HoffQuiz
         {
             InitializeComponent();
         }
+
+        List<TextBox> textBoxes = new();
+
+        private void Reset()
+        {
+            for (int i = 0; i < textBoxes.Count; i++)
+            {
+                stackCreate.Children.Remove(textBoxes[i]);
+            }
+        }
+
+        private void Update()
+        {
+            stackCreate.Children.Clear();
+            for (int i = 0; i < textBoxes.Count; i++)
+            {
+                stackCreate.Children.Add(textBoxes[i]);
+                if (i == 0) textBoxes[i].VerticalAlignment = VerticalAlignment.Center;
+
+            }
+        }
+
+        private void CreateVisual()
+        {
+            TextBox textBox = new();
+            textBox.Padding = new Thickness(5);
+            textBox.Margin = new Thickness(0, 20, 0, 20);
+            textBox.Width = 500;
+            textBox.Text = "Här kan du skriva lite dittan och dattan, test";
+            textBox.HorizontalAlignment = HorizontalAlignment.Center;
+
+            textBoxes.Add(textBox);
+        }
+
+
+        private void Create()
+        {
+            CreateVisual();
+            Update();
+        }
+
+        private void btnNew_Click(object sender, RoutedEventArgs e)
+        {
+            Create();
+        }
     }
 }
