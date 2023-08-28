866b09b928fd82396dc1eae5a27e842aa1ddd325
Grappe88 <leon.h-k@hotmail.com>
2023-08-28 20:13:06 +0200

    1

--- Programmering 2/TicTacToe/Program.cs
+++ Programmering 2/TicTacToe/Program.cs
@@ -16,25 +16,48 @@ namespace TicTacToe
             bool run = true;
             while (run)
             {
-                Console.WriteLine("Vill du spela en runda Tre-I-Rad? - y/n");
-                char answer = Console.ReadKey().KeyChar;
-                Console.WriteLine();
-                if (answer == 'y')
-                {
-
-                }
-                else if (answer == 'n') run = false;
+                run = Choice();
             }
 
             bool Choice()
             {
-                return false;
+                Console.WriteLine("Vill du spela en runda Tre-I-Rad? - y/n/q");
+                char answer = Console.ReadKey().KeyChar;
+                Console.WriteLine();
+
+                if (answer == 'y')
+                {
+                    bool question = true;
+                    while (question)
+                    {
+                        Console.WriteLine("Spelare var vänlig att ange en rad. 1/2/3/q");
+                        char row = Console.ReadKey().KeyChar;
+                        Console.WriteLine();
+                        if (row == 'q') Environment.Exit(0);
+                        else if (int.Parse(row.ToString()) < 4 && int.Parse(row.ToString()) > 0) question = false;
+                    }
+
+                    question = true;
+                    while (question)
+                    {
+                        Console.WriteLine("Spelare var vänlig att ange en kolumn. 1/2/3/q");
+                        char col = Console.ReadKey().KeyChar;
+                        Console.WriteLine();
+                        if (col == 'q') Environment.Exit(0);
+                        else if (int.Parse(col.ToString()) < 4 && int.Parse(col.ToString()) > 0) question = false;
+                        else Console.WriteLine("Du får försöka igen: \r\n");
+                    }
+                }
+                else if (answer == 'n' || answer == 'q') return false;
+                return true;
             }
 
             bool CurrentPlayer()
             {
                 return false;
             }
+
+            Environment.Exit(0);
         }
     }
 }
