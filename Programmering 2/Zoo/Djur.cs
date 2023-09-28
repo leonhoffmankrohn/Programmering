326892b92e9ed87c2ffdbeadb2d23e283672159a
Leon Hoffman-Krohn <0511LEHO@gapp.uddevalla.se>
2023-09-28 13:34:28 +0200

    Del 1 Klar

--- Programmering 2/Zoo/Djur.cs
+++ Programmering 2/Zoo/Djur.cs
@@ -8,15 +8,22 @@ namespace Zoo
 {
     class Djur
     {
+        // Denna kommer att öka för varje skapat djur
         static int id = 0;
-        int ID;
-        string namn;
-        double ålder;
-        KönTyp kön;
 
-        public Djur()
+        // Egenskaperna
+        public int Id { get; }
+        public string Namn { get; }
+        public double Ålder { get; }
+        public KönTyp Kön { get; }
+
+        // Här kommer konstruktören för att skapa ett djur och här ökar den även "id"
+        public Djur(string _namn, double _ålder, KönTyp _kön)
         {
-
+            Id = ++id;
+            Namn = _namn;
+            Ålder = _ålder;
+            Kön = _kön;
         }
 
     }
--- Programmering 2/Zoo/MainWindow.xaml.cs
+++ Programmering 2/Zoo/MainWindow.xaml.cs
@@ -24,25 +24,35 @@ namespace Zoo
         public MainWindow()
         {
             InitializeComponent();
+
+            // Här läggs köntyperna in i listboxen
             string[] köntyper = Enum.GetNames(typeof(KönTyp));
 
             foreach (string kön in köntyper) lbxKön.Items.Add(kön);
+            lbxKön.SelectedIndex = 0;
         }
+
+        // Här uppdateras listview:en med korresponderande data
         void UppdateraLviewRegister()
         {
-
+            lviewRegister.ItemsSource = null;
+            lviewRegister.ItemsSource = djurlista;
         }
 
+        // Här skapas ett djur och läggs till i en djurlista
         void SkapaDjur(string namn, double ålder, KönTyp kön)
         {
-            Djur djur = new() { namn, ålder, kön };
+            djurlista.Add(new Djur(namn, ålder, kön));
         }
 
+        // Denna körs vid knapptryck och samlar in data om djuret och skickar data för skapande och lagrande av djur men åker också vidare till att uppdatera listview:en
         private void btnLäggTillDjur_Click(object sender, RoutedEventArgs e)
         {
             string namn = tbxNamn.Text;
             double.TryParse(tbxÅlder.Text, out double ålder);
-            
+            KönTyp könet = (KönTyp)lbxKön.SelectedIndex;
+            SkapaDjur(namn, ålder, könet);
+            UppdateraLviewRegister();
         }
     }
 }
