932ea02b7c2f259c00cc9b6aa2240a468004de62
Leon Hoffman-Krohn <0511LEHO@gapp.uddevalla.se>
2023-10-23 09:46:37 +0200

    1 (ILäggÄgg klar)

---
+++ Programmering 2/Zoo/Interfaces/ILäggÄgg.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zoo.Interfaces
+{
+    internal interface ILäggÄgg
+    {
+        public double ÄggPerKull {get; set;}
+
+        public string LäggÄgg();
+    }
+}
