036dc64780fdcdd00840ae5d0f4a5002258addd8
Leon Hoffman-Krohn <0511LEHO@gapp.uddevalla.se>
2023-10-23 09:49:14 +0200

    1

--- Programmering 2/Zoo/Klasser/Fiskdjursklasser/Fisk.cs
+++ Programmering 2/Zoo/Klasser/Fiskdjursklasser/Fisk.cs
@@ -3,13 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Zoo.Interfaces;
 
 namespace Zoo
 {
-    abstract internal class Fisk : Djur
+    abstract internal class Fisk : Djur, ILäggÄgg
     {
         // Egenskaper
         public bool Sötvatten { get; }
+        public double ÄggPerKull { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
 
         // Konstruktören
         public Fisk(string _namn, double _ålder, KönTyp _kön, bool _sötvatten) : base(_namn, _ålder, _kön)
@@ -21,6 +23,11 @@ namespace Zoo
         public override string ToString()
         {
             return (Sötvatten) ? "som lever i sötvatten" : "som lever i havet";
+        }
+
+        public string LäggÄgg()
+        {
+            throw new NotImplementedException();
         }
     }
 }
