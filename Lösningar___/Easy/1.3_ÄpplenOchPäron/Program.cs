b3f5ec147f5a4ae7e9616481391287732ae5cf1d
Grappe88 <leon.h-k@hotmail.com>
2023-11-05 17:07:04 +0100

    klar

---
+++ Lösningar___/Easy/1.3_ÄpplenOchPäron/Program.cs
@@ -0,0 +1,12 @@
+﻿
+string input = Console.ReadLine();
+
+int.TryParse(input.Split(' ')[0], out int axelAmount);
+int.TryParse(input.Split(' ')[1], out int petraAmount);
+
+int axelSold = axelAmount * 7;
+int petraSold = petraAmount * 13;
+
+if (axelSold == petraSold) Console.WriteLine("lika");
+else if (axelSold > petraSold) Console.WriteLine("axel");
+else if (axelSold < petraSold) Console.WriteLine("petra");
---
+++ Lösningar___/Easy/Triangelarea/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Triangelarea
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            double.TryParse(input.Split(' ')[0], out double bas);
+            double.TryParse(input.Split(' ')[1], out double höjd);
+            Console.WriteLine(bas * höjd / 2);
+        }
+    }
+}
