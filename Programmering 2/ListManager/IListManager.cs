1b923ce4699df40e68e877d0eea85130a0d6e55e
Leon Hoffman-Krohn <0511LEHO@gapp.uddevalla.se>
2023-12-11 09:23:36 +0100

    Klar ListManager

---
+++ Programmering 2/ListManager/IListManager.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListManager
+{
+    internal interface IListManager<T>
+    {
+        /// <summary>
+        /// Returnerar mängden objekt i listan
+        /// </summary>
+        int Count { get; }
+
+        /// <summary>
+        /// Lägger till ett objekt i listan
+        /// </summary>
+        /// <param name="aType">ett generiskt objekt</param>
+        /// <returns>true om lyckat, false annars</returns>
+        bool Add(T aType);
+
+        /// <summary>
+        /// Returnerar ett specifikt objekt ur listan
+        /// </summary>
+        /// <param name="anIndex">objektets index i listan</param>
+        /// <returns>ett objekt</returns>
+        T GetAt(int anIndex);
+
+        /// <summary>
+        /// Ändrar ett specifikt objekt i listan
+        /// </summary>
+        /// <param name="aType">ett generiskt objekt</param>
+        /// <param name="anIndex">dess plats i listan</param>
+        /// <returns>true om lyckat, false annars</returns>
+        bool ChangeAt(T aType, int anIndex);
+
+        /// <summary>
+        /// Tar bort ett specifikt objekt ur listan
+        /// </summary>
+        /// <param name="anIndex">indexet som ska tas bort</param>
+        /// <returns>true om lyckat, false annars</returns>
+        bool Delete(int anIndex);
+
+        /// <summary>
+        /// Rensar listan
+        /// </summary>
+        void DeleteAll();
+
+        /// <summary>
+        /// Skapar en array med strängar från objektens ToString
+        /// </summary>
+        /// <returns>en array med strängar</returns>
+        string[] ToStringArray();
+    }
+}
+
---
+++ Programmering 2/ListManager/ListManager.cs
@@ -0,0 +1,64 @@
+﻿using System.ComponentModel;
+
+namespace ListManager
+{
+    abstract class ListManager : IListManager<Type>
+    {
+        List<Type> lista;
+        
+        public int Count { get { return lista.Count; } }
+        public ListManager()
+        {
+            lista = new List<Type> ();
+        }
+
+        public virtual bool Add(Type aType)
+        {
+            if (aType == null)
+            {
+                return false;
+            }
+            else
+            {
+                lista.Add(aType);
+                return true;
+            }
+        }
+
+        public virtual Type GetAt(int anIndex)
+        {
+            return lista[anIndex];
+        }
+
+        public virtual bool ChangeAt(Type aType, int anIndex)
+        {
+            if ((anIndex < Count && anIndex > -1) && aType != null)
+            {
+                lista[anIndex] = aType;
+                return true;
+            }
+            else return false;
+        }
+
+        public virtual bool Delete(int anIndex)
+        {
+            if (anIndex < Count && anIndex > -1)
+            {
+                lista.RemoveAt(anIndex);
+                return true;
+            }
+            else return false;
+        }
+
+        public virtual void DeleteAll()
+        {
+            lista.Clear();
+        }
+
+        public virtual string[] ToStringArray()
+        {
+            string[] stringArray = new string[Count];
+            foreach (Type aType in lista) { stringArray.Append(aType.ToString()); }
+            return stringArray;
+    }
+}
