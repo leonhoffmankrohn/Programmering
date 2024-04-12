b93b8cdff164da93d0456d4b7f6ba0fc7317951f
Leon Hoffman-Krohn <0511LEHO@gapp.uddevalla.se>
2024-04-12 15:52:44 +0200

    1

--- Programmering 2/ClientComputer/ClientComputer/Program.cs
+++ Programmering 2/ClientComputer/ClientComputer/Program.cs
@@ -2,16 +2,17 @@
 using System.Net.Sockets;
 using System.Text;
 
-var ipEndPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 12345);
+Console.Title = "Klient";
 
-UdpClient udpClient = new UdpClient();
-while (true)
-{
-    Console.Clear();
-    Console.WriteLine("Server ready for message.");
-    Console.WriteLine("\nWrite something to the server: \n - ");
-    string message = Console.ReadLine();
-    byte[] data = Encoding.Unicode.GetBytes(message);
+IPEndPoint serverpoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 12345);
+TcpClient client = new();
 
-    udpClient.Send(data, data.Length, ipEndPoint);
-}
+client.Connect(serverpoint);
+
+Console.WriteLine("Connected:");
+string message = Console.ReadLine();
+
+byte[] bytes = Encoding.Unicode.GetBytes(message);
+
+client.GetStream().Write(bytes, 0, bytes.Length);
+client.Close();
--- Programmering 2/ServerComputer/ServerComputer/Program.cs
+++ Programmering 2/ServerComputer/ServerComputer/Program.cs
@@ -2,12 +2,20 @@
 using System.Net.Sockets;
 using System.Text;
 
-var clientEndPoint = new IPEndPoint(IPAddress.Any, 0);
-UdpClient client = new UdpClient(80);
+Console.Title = "Server";
+int port = 12345;
 
-Console.WriteLine("Server redo för meddelanden.\n");
-while (true)
-{
-    byte[] indata = client.Receive(ref clientEndPoint);
-    Console.WriteLine(" - " + Encoding.Unicode.GetString(indata));
-}
+IPEndPoint endpoint = new IPEndPoint(IPAddress.Any, port);
+TcpListener listener = new TcpListener(endpoint);
+
+listener.Start();
+TcpClient klient = listener.AcceptTcpClient();
+
+byte[] buffer = new byte[256];
+int count = klient.GetStream().Read(buffer, 0, buffer.Length);
+string message = Encoding.Unicode.GetString(buffer, 0, count);
+
+Console.WriteLine(message);
+
+klient.Close();
+listener.Stop();
