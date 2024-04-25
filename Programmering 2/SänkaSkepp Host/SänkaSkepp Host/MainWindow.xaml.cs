95a95e56f1e4ee0bd24c579163c2e9c10bf76607
Leon Hoffman-Krohn <0511LEHO@gapp.uddevalla.se>
2024-04-25 14:01:50 +0200

    1

---
+++ Programmering 2/SänkaSkepp Host/SänkaSkepp Host/MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SänkaSkepp_Host
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    /// 
+
+    /*
+     * Låta hosten göra sin spelplan
+     * När klienten ansluter börjar hosten sitt första drag
+     * draget evalureras och skickas till klienten
+     * klienten skickar sitt drag och det evalueras på hosten och skickas tillbaka
+     */
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
